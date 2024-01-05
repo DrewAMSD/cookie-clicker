286ba58f820bfd12819db694cfe82bda9ad4ee9c
DrewAMSD <drewalgismiller2@gmail.com>
2024-01-04 23:56:24 -0800

    setup upgrades and stats divs

--- src/pages/CookiePage.jsx
+++ src/pages/CookiePage.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import "./CookiePage.css";
 import cookieImage from "../images/cookie.jpg";
+import cookieImage2 from "../images/cookie2.jpg";
 
 export default function CookiePage() {
   const [cookies, setCookies] = useState(0);
   const [cps, setCps] = useState(0);
+  const [clickStrength, setClickStrength] = useState(1);
+  const [doughRoller, setDoughRoller] = useState([10, 0]);
 
-  const TotalCookies = () => {
+  const CookieStats = () => {
     return (
-      <>
+      <div className="cookie-stats">
         <div>Total Cookies:</div>
         <div className="total-cookies">{cookies}</div>
-        <div>cps: {cps}</div>
-      </>
+        <div>cps:</div>
+        <div>{cps}</div>
+        <div>clickStrength:</div>
+        <div>{clickStrength}</div>
+      </div>
     );
   };
 
@@ -33,17 +39,52 @@ export default function CookiePage() {
     );
   };
 
-  const ClickStrengthUpgarde = () => {};
+  const ClickStrengthUpgradeClicked = () => {
+    if (cookies >= doughRoller[0]) {
+      setCookies((prev) => {
+        return prev - doughRoller[0];
+      });
+      setDoughRoller((prev) => {
+        prev[0] = Math.round(prev[0] * 2.5);
+        prev[1] = prev[1] + 1;
+        return prev;
+      });
+    }
+  };
+
+  const ClickStrengthUpgrade = (props) => {
+    return (
+      <>
+        <div onClick={props.onClick} className="clickStrengthUpgrade-container">
+          <div className="clickStrengthUpgrade-textcost-container">
+            <div className="clickStrengthUpgrade-text">
+              Dough Roller: +1 click strength
+            </div>
+            <div className="clickStrengthUpgrade-cost">
+              <div>cost: {doughRoller[0]}</div>
+              <img
+                className="cookie-picture2"
+                src={cookieImage2}
+                alt="cookie2"
+              ></img>
+            </div>
+          </div>
+          <div className="clickStrengthUpgrade-num-owned">{doughRoller[1]}</div>
+        </div>
+      </>
+    );
+  };
 
   return (
     <>
       <div className="cookie-page">
         <div className="cookie-container">
-          <TotalCookies />
+          <CookieStats />
           <Cookie onClick={CookieClicked} />
         </div>
         <div className="cookie-upgrades">
-          <ClickStrengthUpgrade />
+          <div className="cookie-upgrades-text">Upgrades:</div>
+          <ClickStrengthUpgrade onClick={ClickStrengthUpgradeClicked} />
         </div>
       </div>
     </>
