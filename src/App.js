0675d39d10f0786f0667b7b2fbc297b99e34c31d
DrewAMSD <drewalgismiller2@gmail.com>
2024-01-09 21:20:11 -0800

    setup gh-pages 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
-import CookiePage from "./pages/CookiePage";
+import Home from "./routes/Home";
+import CookiePage from "./routes/CookiePage";
 
 function App() {
   return (
     <>
       <div className="app">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/CookiePage" element={<CookiePage />}></Route>
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/CookiePage" element={<CookiePage />}></Route>
+        </Routes>
       </div>
     </>
   );
